f4df9f2cd269f434343e8628f015f65e454e57c0
taypsl <tayparsell@gmail.com>
2017-09-27 22:50:59 -0700

    updating package.json and webpack to work with bot server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
   context: __dirname,
@@ -10,14 +11,25 @@ const config = {
     filename: 'bundle.js',
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
+    rules: [
+    {
+      // exclude: /node_modules/,
+      exclude: /node_modules(?!\/webpack-dev-server)/,
       test: /\.(js|jsx)$/,
       loader: 'babel-loader',
     },
+    { test: /\.css$/i,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
+      })
+    },
     {
-      test: /\.scss$/,
-      loader: ExtractTextPlugin.extract('css-loader!sass-loader'),
+      test: /\.scss$/i,
+      exclude: /node_modules/,
+      use: ExtractTextPlugin.extract({
+        use: ['css-loader', 'sass-loader']
+      })
     },
     {
       test: /\.(png|jpg|gif)$/,
@@ -28,24 +40,25 @@ const config = {
         }
       ]
     },
-    {
-      test: /\.svg$/,
-      use: [
-        {
-          loader: 'file-loader'
-        },
-        {
-          loader: 'svgo-loader',
-          options: {
-            plugins: [
-              {removeTitle: true},
-              {convertColors: {shorthex: false}},
-              {convertPathData: false}
-            ]
-          }
-        }
-      ]
-    },],
+    // {
+    //   test: /\.svg$/,
+    //   use: [
+    //     {
+    //       loader: 'file-loader'
+    //     },
+    //     {
+    //       loader: 'svgo-loader',
+    //       options: {
+    //         plugins: [
+    //           {removeTitle: true},
+    //           {convertColors: {shorthex: false}},
+    //           {convertPathData: false}
+    //         ]
+    //       }
+    //     }
+    //   ]
+    // },
+    ],
   },
   devServer: {
     historyApiFallback: true,
@@ -55,17 +68,14 @@ const config = {
     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
     // new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-      output: { comments: false },
-      mangle: false,
-      sourcemap: false,
-      minimize: true,
-      mangle: { except: ['$super', '$', 'exports', 'require', '$q', '$ocLazyLoad'] },
+    new UglifyJSPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false,
+        comparisons: false, // don't optimize comparisons
+      }
     }),
-    new ExtractTextPlugin({filename:'src/public/stylesheets/app.css',
-      allChunks: true,
-    }),
+    new ExtractTextPlugin({filename: 'src/public/stylesheets/app.css', allChunks: true})
   ],
 };
 
@@ -88,3 +98,21 @@ module.exports = config;
 //         comments: false,
 //       },
 //     }),
+
+/*
+
+{
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({'css-loader!sass-loader'}),
+    },
+
+new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false },
+      mangle: false,
+      sourcemap: false,
+      minimize: true,
+      mangle: { except: ['$super', '$', 'exports', 'require', '$q', '$ocLazyLoad'] },
+    }),
+
+*/
